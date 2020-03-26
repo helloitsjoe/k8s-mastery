265af033f467d71a82fb94e29447bdfab55f9932
helloitsjoe <jboyle617@gmail.com>
2020-03-26 07:29:30 -0400

    hooks

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -1,61 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
-import Polarity from "./components/Polarity";
+import Polarity from './components/Polarity';
 
 const style = {
-    marginLeft: 12,
+  marginLeft: 12,
 };
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sentence: '',
-            polarity: undefined
-        };
-    };
+function App() {
+  const [{ sentence, polarity }, setState] = React.useState({ sentence: '', polarity: null });
 
-    analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({sentence: this.textField.getValue()})
-        })
-            .then(response => response.json())
-            .then(data => this.setState(data));
+  const textField = React.useRef();
+
+  function analyzeSentence() {
+    fetch('http://localhost:8080/sentiment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ sentence: textField.current.getValue() }),
+    })
+      .then(response => response.json())
+      .then(data => this.setState(data));
+  }
+
+  const onEnterPress = e => {
+    if (e.key === 'Enter') {
+      analyzeSentence();
     }
+  };
 
-    onEnterPress = e => {
-        if (e.key === 'Enter') {
-            this.analyzeSentence();
-        }
-    };
-
-    render() {
-        const polarityComponent = this.state.polarity !== undefined ?
-            <Polarity sentence={this.state.sentence} polarity={this.state.polarity}/> :
-            null;
-
-        return (
-            <MuiThemeProvider>
-                <div className="centerize">
-                    <Paper zDepth={1} className="content">
-                        <h2>Sentiment Analyser</h2>
-                        <TextField ref={ref => this.textField = ref} onKeyUp={this.onEnterPress.bind(this)}
-                                   hintText="Type your sentence."/>
-                        <RaisedButton  label="Send" style={style} onClick={this.analyzeSentence.bind(this)}/>
-                        {polarityComponent}
-                    </Paper>
-                </div>
-            </MuiThemeProvider>
-        );
-    }
+  return (
+    <MuiThemeProvider>
+      <div className="centerize">
+        <Paper zDepth={1} className="content">
+          <h2>Sentiment Analyser</h2>
+          <TextField ref={textField} onKeyUp={onEnterPress} hintText="Type your sentence." />
+          <RaisedButton label="Send" style={style} onClick={analyzeSentence} />
+          {polarity && <Polarity sentence={sentence} polarity={polarity} />}
+        </Paper>
+      </div>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
--- sa-frontend/src/components/Polarity.js
+++ sa-frontend/src/components/Polarity.js
@@ -1,23 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Polarity extends Component {
+function Polarity({ polarity, sentence }) {
+  const green = Math.round((polarity + 1) * 128);
+  const red = 255 - green;
+  const textColor = {
+    backgroundColor: 'rgb(' + red + ', ' + green + ', 0)',
+    padding: '15px',
+  };
 
-    propTypes = {
-        sentence: PropTypes.string.isRequired,
-        polarity: PropTypes.number.isRequired
-    };
-
-    render() {
-        const green = Math.round((this.props.polarity + 1) * 128);
-        const red = 255 - green;
-        const textColor = {
-            backgroundColor: 'rgb(' + red + ', ' + green + ', 0)',
-            padding: '15px'
-        };
-
-        return <div style={textColor}>"{this.props.sentence}" has polarity of {this.props.polarity} </div>
-    }
+  return (
+    <div style={textColor}>
+      "{sentence}" has polarity of {polarity}{' '}
+    </div>
+  );
 }
+Polarity.propTypes = {
+  sentence: PropTypes.string.isRequired,
+  polarity: PropTypes.number.isRequired,
+};
 
 export default Polarity;
